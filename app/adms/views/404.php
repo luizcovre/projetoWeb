d720703502575df5c72fd08818ff63b5cd3a57e1
Luiz Covre <luiz.1covre@gmail.com>
2024-08-05 12:34:13 -0300

    Redireciona login e erro 404

---
+++ app/adms/views/404.php
@@ -0,0 +1,3 @@
+<?php
+    echo "Erro 404: O link que você tentou acessar não está disponível ou não existe..."
+?>
--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
-    $path_page = "login"
+    // Recebe a url
+    $url = filter_input(INPUT_GET, "url", FILTER_SANITIZE_URL);
+
+    if($url == null){
+        $url = "";
+    }
+
+    // Converte a string em array
+    $url_path = explode("/", $url);
+
+    if ((isset($url_path[0])) && (!empty($url_path[0]))) {
+        $path_page = $url_path[0];
+    } else {
+        $path_page = "login";
+    }
+
+    if ((isset($url_path[1])) && (!empty($url_path[1]))) {
+        $path_detail = $url_path[1];
+    } else {
+        $path_detail = "";
+    }
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -13,7 +33,7 @@
                 if(file_exists("app/adms/views/".$path_page.".php")){
                     include_once "app/adms/views/".$path_page.".php";
                 }else{
-                    include_once "app/adms/views/login.php";  
+                    include_once "app/adms/views/404.php";  
                     echo "Página $path_page não existe";
                 }
             }else{

c9049c9fc7d5c98a247f6ee24bf7d6c674fb6e9a
Luiz Covre <luiz.1covre@gmail.com>
2024-08-05 11:42:58 -0300

    Login

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
-    echo"Olá"
+    $path_page = "login"
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Covre</title>
+    </head>
+    <body>
+        <?php
+            if(!empty($path_page)){
+                if(file_exists("app/adms/views/".$path_page.".php")){
+                    include_once "app/adms/views/".$path_page.".php";
+                }else{
+                    include_once "app/adms/views/login.php";  
+                    echo "Página $path_page não existe";
+                }
+            }else{
+                include_once "app/adms/views/login.php";
+            }
+        ?>
+    </body>
+</html>

df775108fee1907bd04edd200fd1cd4ff352ffa2
Luiz Covre <luiz.1covre@gmail.com>
2024-08-06 15:42:19 -0300

    inicio formulário e scripts do DB

--- adm/app/adms/views/login.php
+++ adm/app/adms/views/login.php
@@ -1,3 +1,18 @@
 <?php
-    echo "Login php"
+    if(!defined("A1B2C3")){
+        die("Erro: Página não encontrada!<br>");
+    }
+
+    $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+    var_dump($dados);
 ?>
+
+<form method="POST" action="">
+    <label>Usuário</label>
+    <input type="text" name="username" placeholder="Digite o usuário ou e-mail" autofocus required><br><br>
+
+    <label>Senha</label>
+    <input type="password" name="password" placeholder="Digite a senha" required><br><br>
+
+    <input type="submit" name="envioLogin" value="Acessar">
+</form>
